a6da8475550d61e5dca3f9f5649e615825fde0c6
Neaox <contact@chrisdyson.co.nz>
2015-10-23 10:43:27 +1300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function ObjectPropsEqual(objA, objB) {
+    if (objA === objB) return true;
+    if (!objA && !objB) return true;
+    if (!objA || !objB) return false;
+
+    const objAKeys = Object.keys(objA);
+    const objBKeys = Object.keys(objB);
+
+    if (objAKeys.length !== objBKeys.length) return false;
+
+    return objAKeys.every(key => {
+        return objA[key] === objB[key];
+    });
+};
